d77ec2c036983bfa66f96d6de26b80cebeb10ca8
Miho Krusic <miho.krusic@gmail.com>
2020-12-09 09:55:32 +0100

    Day 9, part 1 and 2

--- solutions/day09.py
+++ solutions/day09.py
@@ -1,5 +1,42 @@
-def part1(input):
-    return 1
+def __get_parts(preamble_numbers, target):
+    for i in range(len(preamble_numbers)):
+        for j in range(i + 1, len(preamble_numbers)):
+            if (preamble_numbers[i] + preamble_numbers[j] == target and preamble_numbers[i] != preamble_numbers[j]):
+                return True
 
-def part2(input):
-    return 2
+    return False
+
+def part1(input, preamble):
+    numbers = [int(x) for x in input]
+    ix = preamble
+    preamble_numbers = numbers[:ix]
+
+    for i in range(preamble, len(input)):
+        if not __get_parts(preamble_numbers, numbers[i]):
+            return numbers[i]
+
+        preamble_numbers.pop(0)
+        preamble_numbers.append(numbers[i])
+
+    return -1
+
+def part2(input, target):
+    numbers = [int(x) for x in input]
+    current = 0
+    numbers_set = set()
+    for i in range(len(numbers)):
+        numbers_set.clear()
+        numbers_set.add(numbers[i])
+        current = numbers[i]
+        for j in range(i + 1, len(numbers)):
+            numbers_set.add(numbers[j])
+            current += numbers[j]
+
+            if current > target:
+                break
+
+            if current == target:
+                sorted_set = sorted(numbers_set)
+                return sorted_set[0] + sorted_set[len(sorted_set) - 1]
+
+    return -1
--- tests/test09.py
+++ tests/test09.py
@@ -7,22 +7,22 @@ from utility import inputs
 class Part1(unittest.TestCase):
     def test_01(self):
         input = inputs.read("input09")
-        result = day09.part1(input)
-        self.assertEqual(result, 1)
+        result = day09.part1(input, 5)
+        self.assertEqual(result, 127)
     def test_in(self):
         input = inputs.read("input09_actual")
-        result = day09.part1(input)
-        self.assertEqual(result, 1)
+        result = day09.part1(input, 25)
+        self.assertEqual(result, 393911906)
 
 class Part2(unittest.TestCase):
     def test_02(self):
         input = inputs.read("input09")
-        result = day09.part2(input)
-        self.assertEqual(result, 2)
+        result = day09.part2(input, 127)
+        self.assertEqual(result, 62)
     def test_in(self):
         input = inputs.read("input09_actual")
-        result = day09.part2(input)
-        self.assertEqual(result, 2)
+        result = day09.part2(input, 393911906)
+        self.assertEqual(result, 59341885)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
