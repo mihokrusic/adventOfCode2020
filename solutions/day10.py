a9e34f3a39195de930f1292873aa2fe93b8e9dd7
Miho Krusic <miho.krusic@gmail.com>
2020-12-10 10:53:59 +0100

    Day 10, part 1 and 2

--- solutions/day10.py
+++ solutions/day10.py
@@ -1,5 +1,38 @@
+import copy
+import os
+
 def part1(input):
-    return 1
+    adapters = sorted([int(x) for x in input])
+
+    current = 0
+    diffs = dict()
+    diffs[3] = 1
+    for i in adapters:
+        diff = i - current
+        current = i
+        if diff in diffs:
+            diffs[diff] += 1
+        else:
+            diffs[diff] = 1
+
+    return diffs[1] * diffs[3]
+
+
+def __calculate_chain(adapters, cache, current, target):
+    next_adapters = [x for x in adapters if 1 <= x - current <= 3 and x != target]
+    sol_here = len([x for x in adapters if 1 <= x - current <= 3 and x == target])
+    for el in next_adapters:
+        if not el in cache:
+            cache[el] = __calculate_chain(adapters, cache, el, target)
+
+        sol_here += cache[el]
+
+    return sol_here
 
 def part2(input):
-    return 2
+    adapters = sorted([int(x) for x in input])
+    target = adapters[len(adapters) - 1]
+    cache = dict()
+
+    solutions = __calculate_chain(adapters, cache, 0, target)
+    return solutions
--- tests/test10.py
+++ tests/test10.py
@@ -6,23 +6,31 @@ from utility import inputs
 
 class Part1(unittest.TestCase):
     def test_01(self):
-        input = inputs.read("input10")
+        input = inputs.read("input10_1")
         result = day10.part1(input)
-        self.assertEqual(result, 1)
+        self.assertEqual(result, 35)
+    def test_02(self):
+        input = inputs.read("input10_2")
+        result = day10.part1(input)
+        self.assertEqual(result, 220)
     def test_in(self):
         input = inputs.read("input10_actual")
         result = day10.part1(input)
-        self.assertEqual(result, 1)
+        self.assertEqual(result, 2170)
 
 class Part2(unittest.TestCase):
-    def test_02(self):
-        input = inputs.read("input10")
+    def test_01(self):
+        input = inputs.read("input10_1")
         result = day10.part2(input)
-        self.assertEqual(result, 2)
+        self.assertEqual(result, 8)
+    def test_02(self):
+        input = inputs.read("input10_2")
+        result = day10.part2(input)
+        self.assertEqual(result, 19208)
     def test_in(self):
         input = inputs.read("input10_actual")
         result = day10.part2(input)
-        self.assertEqual(result, 2)
+        self.assertEqual(result, 24803586664192)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
