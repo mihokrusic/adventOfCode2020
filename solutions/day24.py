19f6aee39d792a2229cadb19dd3bb9fdf4942c9b
Miho Krusic <miho.krusic@gmail.com>
2020-12-24 14:52:37 +0100

    Day 24, part 2 5sec

--- solutions/day24.py
+++ solutions/day24.py
@@ -10,39 +10,31 @@ movements = {
     'ne': [1, 0, -1],
 }
 
-def get_blacks(field):
-    blacks = 0
-    for key in field.keys():
-        if field[key] == 1:
-            blacks+=1
-    return blacks
-
 def get_black_adjacent(field, pos):
     result = 0
     adj_key = (pos[0] + 1, pos[1] - 1, pos[2])
-    if adj_key in field and field[adj_key] == 1:
+    if adj_key in field:
         result += 1
     adj_key = (pos[0] - 1, pos[1] + 1, pos[2])
-    if adj_key in field and field[adj_key] == 1:
+    if adj_key in field:
         result += 1
     adj_key = (pos[0] + 1, pos[1], pos[2] - 1)
-    if adj_key in field and field[adj_key] == 1:
+    if adj_key in field:
         result += 1
     adj_key = (pos[0] - 1, pos[1], pos[2] + 1)
-    if adj_key in field and field[adj_key] == 1:
+    if adj_key in field:
         result += 1
     adj_key = (pos[0], pos[1] - 1, pos[2] + 1)
-    if adj_key in field and field[adj_key] == 1:
+    if adj_key in field:
         result += 1
     adj_key = (pos[0], pos[1] + 1, pos[2] - 1)
-    if adj_key in field and field[adj_key] == 1:
+    if adj_key in field:
         result += 1
     return result
 
 
 def solve(input, part):
-    field = dict()
-    dims = np.zeros(6, dtype=int)
+    field = set()
     for line in input.split('\n'):
         # x, y, z
         pos = np.array([0, 0, 0])
@@ -60,35 +52,52 @@ def solve(input, part):
             ix += move_ix
 
         key = tuple(pos)
-        field[key] = 1 if key not in field else  1 - field[key]
-
-        dims[0] = min(pos[0], dims[0])
-        dims[1] = max(pos[0], dims[1])
-        dims[2] = min(pos[1], dims[2])
-        dims[3] = max(pos[1], dims[3])
-        dims[4] = min(pos[2], dims[4])
-        dims[5] = max(pos[2], dims[5])
+        if key not in field:
+            field.add(key)
+        else:
+            field.remove(key)
 
     if part == 1:
-        return get_blacks(field)
-
+        return len(field)
     for ix in range(100):
         new_field = copy.deepcopy(field)
-        for x in range(dims[0] - 1, dims[1] + 2):
-            for y in range(dims[2] - 1, dims[3] + 2):
-                for z in range(dims[4] - 1, dims[5] + 2):
-                    pos = (x, y, z)
-                    blacks_adjacent = get_black_adjacent(field, pos)
-                    if (pos not in field or field[pos] == 0) and blacks_adjacent == 2:
-                        new_field[pos] = 1
-                    if pos in field and field[pos] == 1 and (blacks_adjacent == 0 or blacks_adjacent > 2):
-                        new_field[pos] = 0
+        for key in field:
+            blacks_adjacent = get_black_adjacent(field, key)
+            if key not in field and blacks_adjacent == 2:
+                field.add(key)
+            else:
+                if key in field and (blacks_adjacent == 0 or blacks_adjacent > 2):
+                    new_field.remove(key)
+                adj_key = (key[0] + 1, key[1] - 1, key[2])
+                if adj_key not in field:
+                    blacks_adjacent = get_black_adjacent(field, adj_key)
+                    if blacks_adjacent == 2:
+                        new_field.add(adj_key)
+                adj_key = (key[0] - 1, key[1] + 1, key[2])
+                if adj_key not in field:
+                    blacks_adjacent = get_black_adjacent(field, adj_key)
+                    if blacks_adjacent == 2:
+                        new_field.add(adj_key)
+                adj_key = (key[0] + 1, key[1], key[2] - 1)
+                if adj_key not in field:
+                    blacks_adjacent = get_black_adjacent(field, adj_key)
+                    if blacks_adjacent == 2:
+                        new_field.add(adj_key)
+                adj_key = (key[0] - 1, key[1], key[2] + 1)
+                if adj_key not in field:
+                    blacks_adjacent = get_black_adjacent(field, adj_key)
+                    if blacks_adjacent == 2:
+                        new_field.add(adj_key)
+                adj_key = (key[0], key[1] - 1, key[2] + 1)
+                if adj_key not in field:
+                    blacks_adjacent = get_black_adjacent(field, adj_key)
+                    if blacks_adjacent == 2:
+                        new_field.add(adj_key)
+                adj_key = (key[0], key[1] + 1, key[2] - 1)
+                if adj_key not in field:
+                    blacks_adjacent = get_black_adjacent(field, adj_key)
+                    if blacks_adjacent == 2:
+                        new_field.add(adj_key)
 
-        # dims[0] = dims[0] - 1
-        # dims[1] = dims[1] + 1
-        # dims[2] = dims[2] - 1
-        # dims[3] = dims[3] + 1
-        # dims[4] = dims[4] - 1
-        # dims[5] = dims[5] + 1
         field = new_field
-    return get_blacks(field)
+    return len(field)
--- tests/test24.py
+++ tests/test24.py
@@ -19,9 +19,9 @@ class Part2(unittest.TestCase):
     def test_01(self):
         result = day24.solve(inputs.read_str("input24"), 2)
         self.assertEqual(result, 2208)
-    # def test_in(self):
-    #     result = day24.solve(inputs.read_str("input24_actual"), 2)
-    #     self.assertEqual(result, 4133)
+    def test_in(self):
+        result = day24.solve(inputs.read_str("input24_actual"), 2)
+        self.assertEqual(result, 4133)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
